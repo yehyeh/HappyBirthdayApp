45c1686c35ad6fd5ab9343d523ed9224fb5e445d
YehYeh <yehyeh@users.noreply.github.com>
2024-05-13 14:53:31 +0300

    Organized Project folder

--- HappyBirthdayApp/Modules/Input Screen/InputViewController.swift
+++ HappyBirthdayApp/Modules/Input Screen/InputViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  InputViewController.swift
 //  HappyBirthdayApp
 //
 //  Created by Yehonatan Yehudai on 13/05/2024.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class InputViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
