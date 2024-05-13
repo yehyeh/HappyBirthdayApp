135f1d7298fc087650d6bc588e9c43da6d28d45d
YehYeh <yehyeh@users.noreply.github.com>
2024-05-13 14:26:37 +0300

    Initial Commit

---
+++ HappyBirthdayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HappyBirthdayApp
+//
+//  Created by Yehonatan Yehudai on 13/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
